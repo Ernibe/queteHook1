0493d35d860add6f8f6b20e34060bb8ffe75f210
Ornella GRISTI <ornellazaidi@gmail.com>
2019-11-12 16:14:20 +0100

    quete hooks

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  let handleChange = (event) =>{
+    setCount(Number(event.target.value));}
+  
+  return (
+    <div>
+      <p>Le compteur est  à : {count} </p>
+      <input type= "number" value={count} onChange={handleChange}></input>
+      <button onClick={() => setCount (count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
